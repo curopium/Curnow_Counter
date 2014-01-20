f6391c775f677f9e3c0a0ddccef77b714ac34688
curopium <curnow@ualberta.ca>
2014-01-19 20:33:00 -0700

    First commit

---
+++ src/ca/ualberta/cs/curnow_counter/Curnow_CounterActivity.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.curnow_counter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Curnow_CounterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_curnow__counter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.curnow__counter, menu);
+		return true;
+	}
+
+}

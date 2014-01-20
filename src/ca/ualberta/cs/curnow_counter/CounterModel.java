4a75f5f57d82b207ce7f677bcbb185ef732903c2
curopium <curnow@ualberta.ca>
2014-01-20 00:02:51 -0700

    Added in base of counter model

---
+++ src/ca/ualberta/cs/curnow_counter/CounterModel.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.curnow_counter;
+
+import java.util.Date;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CounterModel {
+
+	private Button button;
+	private Date timestamp;
+	private TextView text;
+	private int button_value;
+	
+	
+	public CounterModel() {
+		
+	}
+	
+}

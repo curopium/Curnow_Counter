8abf7813555eb72a750f218de6e71bf8a61a5b7a
Thomas Curnow <curnow@ualberta.ca>
2014-01-23 15:34:20 -0700

    Added in Counter List Model

---
+++ src/ca/ualberta/cs/curnow_counter/CounterListModel.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.curnow_counter;
+
+public class CounterListModel {
+	
+	private CounterModel[] array;
+
+	
+	
+	public CounterListModel() {
+		super();
+	}
+
+	public CounterModel[] getArray() {
+		return array;
+	}
+
+	public void setArray(CounterModel[] array) {
+		this.array = array;
+	}
+	
+	
+
+}
